a542a0d7dd446cee98d93470b57e13d5c71df7b2
samiksha12n <samikshaunesarkar@gmail.com>
2023-12-26 15:38:39 +0530

    Assignment

---
+++ Controllers/CricketController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day14Assignment.Controllers
+{
+    public class CricketController : Controller
+    {
+        // GET: Cricket
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult PView()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Day14Assignment.MvcApplication" Language="C#" %>
